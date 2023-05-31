cafbd26096738b9d7b243c6b6a560994f9228715
minoolee <yaqoubiamina@gmail.com>
2023-05-31 23:08:47 +0200

    just start, some changes, more work

--- main.py
+++ main.py
@@ -19,6 +19,37 @@ class Car:
         return f"Brand: {self.brand}\nModel: {self.model}\nBody Type: {self.body}\nFuel Type:" \
                f" {self.fuel}\nPrice: {self.price} $"
 
+
+# Searchin class for menu
+class Search:
+    def __init__(self):
+        self.brand = input("Brand: ")
+        self.model = input("Model: ")
+        self.body = input("Body Type: ")
+        self.fuel = input("Fuel Type: ")
+        self.price = input("Price: ")
+        self.form = [self.brand, self.model, self.body, self.fuel, self.price]
+
+
+    def show_info(self):
+        for info in self.form:
+            print(info)
+
+    def __str__(self):
+        return f"Brand: {self.brand}\nModel: {self.model}\nBody Type: {self.body}\nFuel Type:" \
+               f" {self.fuel}\nPrice: {self.price} $"
+
+# class to buy car
+class Paypale:
+    def __init__(self):
+        self.first_name = input("First Name: ")
+        self.last_name = input("Last Name: ")
+        self.tel = input("Tel: ")
+        self.iban = input("IBAN: ")
+
+    def __str__(self):
+        return f"{self.first_name}\n{self.last_name}\n{self.tel}\n{self.iban}"
+
 class Menu:
     def __init__(self):
         self.car_list = []
@@ -51,17 +82,27 @@ class UserMenu(Menu):
     def remove_item(self, item):
         super().remove_item(item)
 
-    def search_item(self, item):
+    def search_item(self,):
+        search_info = Search()
+        # search_info.show_info()
         found_cars = []
-        for car in self.menu.car_list:
-            if item.lower() == car.brand.lower():
+        for info in search_info.form:
+         for car in self.menu.car_list:
+            if info.lower() == car.brand.lower() or info.lower() == car.model.lower() or\
+                    info.lower() == car.body.lower() or info.lower() == car.fuel.lower():
                 found_cars.append(car)
         if found_cars:
-            print(f"Found {len(found_cars)} car(s) matching the brand '{item}':")
+            print(f"Found {len(found_cars)} car(s) matching the info':")
             for car in found_cars:
                 print(car)
         else:
             print("No cars found.")
+
+
+    def buy_car(self):
+       card = Paypale()
+
+
 
     def __str__(self):
         return super().__str__()
@@ -83,4 +124,6 @@ user = UserMenu(menu)
 
 # user.show_display()
 user.show_menu()
-user.search_item("bmw")
+# user.search_item()
+user.buy_car()
+
