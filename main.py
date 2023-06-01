4fad79b78be0fd956cf9e92899a790134aea2efd
minoolee <yaqoubiamina@gmail.com>
2023-06-01 21:52:49 +0200

    just start, some changes, more work, left some comments

--- main.py
+++ main.py
@@ -8,12 +8,13 @@
 
 
 class Car:
-    def __init__(self, brand, model, body, fuel, price):
+    def __init__(self, brand, model, body, fuel, price, purpose):
         self.brand = brand
         self.model = model
         self.body = body
         self.fuel = fuel
         self.price = price
+        self.purpose = purpose
 
     def __str__(self):
         return f"Brand: {self.brand}\nModel: {self.model}\nBody Type: {self.body}\nFuel Type:" \
@@ -30,7 +31,6 @@ class Search:
         self.price = input("Price: ")
         self.form = [self.brand, self.model, self.body, self.fuel, self.price]
 
-
     def show_info(self):
         for info in self.form:
             print(info)
@@ -39,6 +39,7 @@ class Search:
         return f"Brand: {self.brand}\nModel: {self.model}\nBody Type: {self.body}\nFuel Type:" \
                f" {self.fuel}\nPrice: {self.price} $"
 
+
 # class to buy car
 class Paypale:
     def __init__(self):
@@ -46,9 +47,17 @@ class Paypale:
         self.last_name = input("Last Name: ")
         self.tel = input("Tel: ")
         self.iban = input("IBAN: ")
+        self.price = input("Price: ")
+        self.purpose = input("Purpose: ")
+        self.card_form = [self.first_name, self.last_name, self.price]
+
+    def show_card_info(self):
+        for card in self.card_form:
+            print(card)
 
     def __str__(self):
         return f"{self.first_name}\n{self.last_name}\n{self.tel}\n{self.iban}"
+
 
 class Menu:
     def __init__(self):
@@ -87,10 +96,15 @@ class UserMenu(Menu):
         # search_info.show_info()
         found_cars = []
         for info in search_info.form:
-         for car in self.menu.car_list:
-            if info.lower() == car.brand.lower() or info.lower() == car.model.lower() or\
-                    info.lower() == car.body.lower() or info.lower() == car.fuel.lower():
-                found_cars.append(car)
+             for car in self.menu.car_list:
+                if (
+                    info.lower() == car.brand.lower()
+                    or info.lower() == car.model.lower()
+                    or info.lower() == car.body.lower()
+                    or info.lower() == car.fuel.lower()
+                    or info >= car.price
+                ):
+                    found_cars.append(car)
         if found_cars:
             print(f"Found {len(found_cars)} car(s) matching the info':")
             for car in found_cars:
@@ -98,32 +112,59 @@ class UserMenu(Menu):
         else:
             print("No cars found.")
 
-
-    def buy_car(self):
-       card = Paypale()
-
-
+    def buy_car(self, desired_car_brand, desired_car_model):
+        for car in self.menu.car_list:
+            if (
+                desired_car_brand.lower() == car.brand.lower()
+                and desired_car_model.lower() == car.model.lower()
+            ):
+                print("Car Info \n", car, "\nPurpose: ",  car.purpose)
+                pay = Paypale()
+                if car.purpose == pay.purpose:
+                    print(f"Congratulation {pay.first_name} The {car.brand}, {car.model} is your!!!")
+                    self.menu.car_list.remove(car)
+                else:
+                    print("Something went wrong")
+                break
+        else:
+            print("Car not found")
 
     def __str__(self):
         return super().__str__()
 
 
-bmw = Car("BMW", "i7", "SEDAN", "Diesel", "878787")
-car2 = Car("Mercedes", "h3", "SPORTS CAR", "Diesel", "8768677")
-jeep = Car("Honda", "k3", "CONVERTIBLE", "Diesel", "98799789")
+bmw = Car("BMW", "i7", "SEDAN", "Diesel", "12", "z67j")
+car2 = Car("Mercedes", "h3", "SPORTS CAR", "Diesel", "87", "kj8h")
+jeep = Car("Honda", "k3", "CONVERTIBLE", "Diesel", "98", "ui979")
 
 menu = Menu()
 menu.add_item(car2)
 menu.add_item(bmw)
 menu.add_item(jeep)
 
-# car_menu = menu
-
 user = UserMenu(menu)
-# user.add_item(bmw)
 
-# user.show_display()
-user.show_menu()
-# user.search_item()
-user.buy_car()
 
+while True:
+    print("\nAuto Scout")
+    print("1. Show available cars")
+    print("2. Search for a car")
+    print("3. Buy a car")
+    print("4. Exit")
+
+    choice = input("Enter your choice (1-4): ")
+
+    if choice == "1":
+        user.show_menu()
+    elif choice == "2":
+        user.search_item()
+    elif choice == "3":
+        desired_brand = input("Enter the brand of the car you want to buy: ")
+        desired_model = input("Enter the model of the car you want to buy: ")
+        user.buy_car(desired_brand, desired_model)
+    elif choice == "4":
+        break
+    else:
+        print("Invalid choice. Please try again.")
+
+print("Goodbye!")
